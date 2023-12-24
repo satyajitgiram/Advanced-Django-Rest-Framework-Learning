7d7879817345f06346f25ccd8d72b5b283210856
satyajitgiram <satyajitgiram2000@gmail.com>
2023-12-24 14:58:10 +0530

    added send mail functionality with attachment

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,14 @@
-from django.core.mail import send_mail, mail_admins, BadHeaderError
+from django.core.mail import BadHeaderError, EmailMessage
 from django.shortcuts import render
 
 
 def say_hello(request):
     try:
-        send_mail('subject','message','satyajitgiram2000@gmail.com',['satyajitgiram9545@gmail.com'])
+        mail = EmailMessage("Subject","Body",'from@gmail.com',['to@gmail.com'])
+        mail.attach_file('playground/static/images/bmw.jpeg')
+        mail.send()
+        print("MAIL SENT")
     except BadHeaderError:
+        print("AN ERROR OCCURED")
         pass
     return render(request, 'hello.html', {'name': 'Mosh'})
