d2d0c5022ee54c59caff3d815a56d8c186899b38
satyajitgiram <satyajitgiram2000@gmail.com>
2023-12-25 12:03:26 +0530

    add tests with multiple assert

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,4 +1,5 @@
 from rest_framework.test import APIClient
+from django.contrib.auth.models import User
 from rest_framework import status
 import pytest
 
@@ -17,3 +18,27 @@ class TestCreateCollection:
 
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_data_is_invalid_return_400(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response =  client.post('/store/collections/', {'title':''})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+        
+    def test_if_user_is_not_admin_return_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response =  client.post('/store/collections/', {'title':'a'})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+    def test_if_data_is_valid_return_201(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response =  client.post('/store/collections/', {'title':'a'})
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
